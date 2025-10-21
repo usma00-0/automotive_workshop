77c68cd937c2e22e1c9b4c33a41afd8e785f8bf8
usma00-00 <usmajuan0012@gmail.com>
2025-10-21 15:33:04 -0500

    Mi primer versión api tallerautomotrizcon saludo "hola campeones"

---
+++ src/main/java/co/edu/umanizales/tallerautomotriz_api/TallerautomotrizApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.tallerautomotriz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerautomotrizApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerautomotrizApiApplication.class, args);
+    }
+
+}
