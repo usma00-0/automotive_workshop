17c8d5c6aa04ce97345969905cc3c791240e078e
usma00-00 <usmajuan0012@gmail.com>
2025-11-17 00:42:53 -0500

    Corrección nombres

--- src/main/java/co/edu/umanizales/automotiveworkshop_api/automotiveworkshopApiApplication.java
+++ src/main/java/co/edu/umanizales/automotiveworkshop_api/automotiveworkshopApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TallerautomotrizApiApplication {
+public class automotiveworkshopApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TallerautomotrizApiApplication.class, args);
+        SpringApplication.run(automotiveworkshopApiApplication.class, args);
     }
 
 }
